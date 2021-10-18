17b143b5417e14c80169f06ba8b20f958819287e
onepullnixxa <briseno.chris@gmail.com>
2021-10-18 16:58:33 -0400

    adds footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,9 @@ function Footer(props) {
   return (
     <footer>
       <div>
-        <h1>Footer</h1>
+        <h1>BY</h1>
+        <h1> CHRISTOPHER BRISEÑO</h1>
+        <h1> CREATED IN 2021</h1>
       </div>
     </footer>
   );
